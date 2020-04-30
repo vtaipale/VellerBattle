b2e6efc9a7efbd976eef2cf741e2916187e8f285
vtaipale <valtter.taipale@gmail.com>
2020-04-30 04:34:13 +0300

    Very basic mousemove
    But you can order Fedfleet to move!

--- Assets/Scripts/UI_FleetScanner.cs
+++ Assets/Scripts/UI_FleetScanner.cs
@@ -8,7 +8,7 @@ using UnityEngine.EventSystems;
 public class UI_FleetScanner : MonoBehaviour {
 
 	public Fleet[] AllFleets;
-
+    public Camera TheCamera;
 	public Canvas TheCanvas;
 	public GraphicRaycaster TheRaycaster;
 
@@ -16,35 +16,73 @@ public class UI_FleetScanner : MonoBehaviour {
 
 	EventSystem m_EventSystem;
 
+    public float m_DistanceZ;
+    Plane m_Plane;
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		AllFleets = FindObjectsOfType<Fleet> ();
-		TheCanvas = FindObjectOfType<Canvas> ();
-		TheRaycaster = TheCanvas.GetComponent<GraphicRaycaster> ();
+		//TheCanvas = FindObjectOfType<Canvas> ();
+		//TheRaycaster = TheCanvas.GetComponent<GraphicRaycaster> ();
 
 		//Fetch the current EventSystem. Make sure your Scene has one.
 		m_EventSystem = EventSystem.current;
-	}
+
+        //Create a new plane with normal (0,0,1) at the position away from the camera you define in the Inspector. This is the plane that you can click so make sure it is reachable.
+        //m_Plane = new Plane(Vector3.forward, 7);
+        m_Plane = new Plane(AllFleets[0].Leader.transform.up, 1f);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
-		Camera TheCamera = FindObjectOfType<Camera> ();
+		//Camera TheCamera = FindObjectOfType<Camera> ();
 		//Camera TheCamera = TheRaycaster.eventCamera;
 
-		RaycastHit hit;
-		/*Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+		
 
-		if (Physics.Raycast(ray, out hit)) {
-			Transform objectHit = hit.transform;
+        //Detect when there is a mouse click
+        if (Input.GetMouseButton(0))
+        {
+            //Create a ray from the Mouse click position
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-			if (objectHit.GetComponent<FleetSymbol>() != null)
-			{
-				Debug.Log ("JUUJAAAA"); 
-			}
-		}*/
-	}
+            //Initialise the enter variable
+            float enter = 0.0f;
+
+            if (m_Plane.Raycast(ray, out enter))
+            {
+                //Get the point that is clicked
+                Vector3 hitPoint = ray.GetPoint(enter);
+                Debug.Log("OOO HIT");
+
+                Debug.DrawLine(AllFleets[0].Leader.transform.position, hitPoint, Color.blue);
+
+                //Assing movement order
+                //TODO: vertical boost!
+
+                AllFleets[0].MoveOrderAll(hitPoint);
+            }
+        }
+        else
+        {
+            RaycastHit hit;
+            Ray ray = TheCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, 5))
+            {
+                Transform objectHit = hit.transform;
+
+                Debug.Log("JUUJAAAA" + objectHit.name);
+
+                if (objectHit.GetComponent<FleetSymbol>() != null)
+                {
+                    Debug.Log("UUJEE");
+                }
+            }
+        }
+
+    }
 
 }
